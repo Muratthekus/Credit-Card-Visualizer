733bbd21cc102cd9f44dc75ae901e2d81db3d2da
murat.kus <murat.kus@ceng.deu.edu.tr>
2021-01-08 16:58:23 +0300

    motion layout onClick trigger added

--- view/src/main/java/me/thekusch/view/CreditCardVisualize.kt
+++ view/src/main/java/me/thekusch/view/CreditCardVisualize.kt
@@ -21,17 +21,13 @@ class CreditCardVisualize @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = R.attr.creditCardVisualize
-): ConstraintLayout(context, attrs, defStyleAttr),View.OnTouchListener{
+): MotionLayout(context, attrs, defStyleAttr){
 
     private val binding: CreditCardVisualizeBinding = CreditCardVisualizeBinding.inflate(
         LayoutInflater.from(context),
         this, true
     )
 
-    private var x1=0
-    private var x2 = 0
-
-    private lateinit var  gestureDetectorCompat: GestureDetectorCompat
 
     private var _cardNumberHint = "ENTER CART NUMBER"
     private var _cardUserNameHint = "ENTER CART USER'S NAME"
@@ -53,65 +49,10 @@ class CreditCardVisualize @JvmOverloads constructor(
         with(binding) {
             cardUserName.hint = _cardUserNameHint
             cardNumber.hint = _cardNumberHint
-
         }
 
-        gestureDetectorCompat = GestureDetectorCompat(context,GestureListener())
     }
 
-    override fun onTouchEvent(event: MotionEvent?): Boolean {
-        Log.d("DENEMELER","NABERSİN")
-        when(event?.action) {
-            MotionEvent.ACTION_DOWN -> {
-                x1 = event.x.toInt()
-                return true
-            }
-            MotionEvent.ACTION_UP -> {
-                x2 = event.x.toInt()
-                performClick()
-                return true
-            }
-        }
-        return false
-    }
-
-    override fun performClick(): Boolean {
-        super.performClick()
-        if(x2 > x1) {
-            swipeLeft()
-        }
-        return true
-    }
-
-
-   inner class GestureListener: GestureDetector.SimpleOnGestureListener() {
-        override fun onDown(e: MotionEvent?): Boolean {
-            return true
-        }
-
-        override fun onFling(
-            e1: MotionEvent?,
-            e2: MotionEvent?,
-            velocityX: Float,
-            velocityY: Float
-        ): Boolean {
-            var diffY: Float? = e2?.y?.minus(e1?.y!!)
-            var diffX: Float? = e2?.x?.minus(e1?.x!!)
-            if(abs(diffX!!) > 100 && velocityX > 100) {
-                if(diffX >0) {
-                    swipeLeft()
-                    return true
-                }
-            }
-            return false
-        }
-    }
-    private fun swipeLeft() {
-        ObjectAnimator.ofFloat(binding.detailWrapperFront,"scaleX",-1f).apply {
-            duration = 2000
-            start()
-        }
-    }
 
     private fun obtainStyledAttributes(attrs: AttributeSet?, defStyleAttr: Int) {
         val typedArray = context.theme.obtainStyledAttributes(
@@ -141,7 +82,4 @@ class CreditCardVisualize @JvmOverloads constructor(
         return this.text.toString()
     }
 
-    override fun onTouch(v: View?, event: MotionEvent?): Boolean {
-        return gestureDetectorCompat.onTouchEvent(event)
-    }
 }
